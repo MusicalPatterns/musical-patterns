dea4eb1acbfa0e3c5340b502b4e9de5cfe5e5736
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-01 13:20:08 -0700

    remove the muted 1s, introduce in-between backbony sing song

--- songs/umowchuwowiestCrossedWithStepwise/notes.js
+++ songs/umowchuwowiestCrossedWithStepwise/notes.js
@@ -14,6 +14,13 @@ const umowchuwowiestNote = harmonic => ({
     gain: harmonic === 0 ? 0 : 1,
 })
 
+const inbetweenNote = ([harmonic, duration]) => ({
+    duration,
+    pitch: harmonic,
+    sustain: duration - 0.1,
+    gain: 1,
+})
+
 const umow = [0, 2, 3, 0, 2, 3, 0, 2, 3, 0, 2, 3, 0, 2, 3]
 
 const chuwow = [0, 2, 3, 0, 2, 3, 4, 0, 2, 3, 0, 2, 3, 4, 5]
@@ -37,6 +44,10 @@ const umowwwAltNotes = [1, 0, 3, 1, 0, 3, 1, 0, 3, 1, 0, 3, 1, 0, 3, 1, 0, 3, 1,
 const umowwwAltAndNotNotes = [0, 2, 3, 1, 0, 3, 0, 2, 3, 1, 0, 3, 0, 2, 3, 1, 0, 3, 0, 2, 3, 1, 0, 3].map(umowchuwowiestNote)
 
 const umowwwRestNotes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(umowchuwowiestNote)
+
+const umowWithBoom = [1, 2, 3, 0, 2, 3, 0, 2, 3, 0, 2, 3, 0, 2, 3]
+
+const iestWithBoom = [1, 2, 3, 4, 5, 0, 2, 3, 4, 5, 0, 2, 3, 4, 5]
 
 const umowchuwowiestNotes = sequence([
     chuwow,
@@ -70,7 +81,7 @@ const umowchuwowiestAltNotes = sequence([
 
 const stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes = [
     3, 4, 3, 2, 1, 2,
-    3, 4, 3, 2, 0, 2,
+    3, 4, 3, 2, 1, 2,
     3, 4, 3, 2, 1, 2,
     3, 4, 3, 2, 3,
     4, 5, 6,
@@ -79,13 +90,29 @@ const stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes = [
     // that lasts two bars
     9, 7, 5, 3,
     1, 3, 5, 7, 5, 3,
-    0, 3, 5, 7, 5, 3,
+    1, 3, 5, 7, 5, 3,
     1, 3, 5, 7, 5, 3,
 ].map(stepwiseForUmowchuwowiestNote)
 
+const stepwiseForUmowchuwowiestCrossedWithStepwiseInbetweenNotes = [
+    [3, 1], [3, 2], [4, 1], [4, 2], [4, 1], [3, 2], [3, 1], [2, 2], [1, 1], [2, 2],
+    [3, 1], [3, 2], [4, 1], [4, 2], [4, 1], [3, 2], [3, 1], [2, 2], [1, 1], [2, 2],
+    [3, 1], [3, 2], [4, 1], [4, 2], [4, 1], [3, 2], [3, 1], [2, 2], [1, 1], [2, 2],
+    [3, 1], [3, 2], [4, 1], [4, 2], [4, 1], [3, 2], [3, 1], [2, 2], [3, 1], [3, 2],
+    [4, 1], [4, 2], [4, 1], [5, 2], [5, 1], [5, 2], [6, 1], [6, 2], [6, 1], [6, 2],
+    [7, 1], [7, 2], [7, 1], [7, 2], [7, 1], [8, 2], [8, 1], [8, 2], [8, 1], [8, 2],
+    [9, 1], [9, 2], [9, 1], [9, 2], [9, 1], [9, 2], [10, 1], [10, 2], [10, 1], [10, 2],
+    [10, 1], [10, 2], [10, 1], [11, 2], [11, 1], [11, 2], [11, 1], [11, 2], [11, 1], [11, 2],
+
+    [9, 1], [9, 3], [9, 1], [9, 3], [9, 1], [7, 3], [7, 1], [7, 3], [5, 1], [5, 3], [5, 1], [3, 3],
+    [1, 1], [3, 3], [5, 1], [5, 3], [5, 1], [7, 3], [7, 1], [7, 3], [5, 1], [5, 3], [5, 1], [3, 3],
+    [1, 1], [3, 3], [5, 1], [5, 3], [5, 1], [7, 3], [7, 1], [7, 3], [5, 1], [5, 3], [5, 1], [3, 3],
+    [1, 1], [3, 3], [5, 1], [5, 3], [5, 1], [7, 3], [7, 1], [7, 3], [5, 1], [5, 3], [5, 1], [3, 3],
+].map(inbetweenNote)
+
 const stepwiseForUmowchuwowiestCrossedWithStepwiseUmowchuwowiestNotes = [
     3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1, 2, 2,
-    3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 2, 2, 0, 2, 2,
+    3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1, 2, 2,
     3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 2, 2, 1, 2, 2,
     3, 3, 3, 4, 4, 4, 4, 3, 3, 3, 2, 2, 3, 3, 3,
     4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6,
@@ -94,7 +121,7 @@ const stepwiseForUmowchuwowiestCrossedWithStepwiseUmowchuwowiestNotes = [
     10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
     9, 9, 9, 9, 9, 9, 9, 9, 9, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 3, 3, 3,
     1, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 3, 3, 3,
-    0, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 3, 3, 3,
+    1, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 3, 3, 3,
     1, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5, 3, 3, 3,
 ].map(umowchuwowiestNote)
 
@@ -130,45 +157,57 @@ const rightNumberOfBackboneNotes = sequence([
 ])
 
 const umowchuwowiestCrossedWithStepwiseOneNotes = sequence([
+    // season 1
     umowwwNotes,
     umowchuwowiestNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
+    // season 2
     umowwwRestNotes,
     umowchuwowiestAltNotes,
     umowchuwowiestAltNotes,
+    // season 3
     umowwwRestNotes,
     rightNumberOfBackboneNotes,
     rightNumberOfBackboneNotes,
+    // season 4
     umowwwRestNotes,
     umowchuwowiestNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
 ])
 const umowchuwowiestCrossedWithStepwiseTwoNotes = sequence([
+    // season 1
     umowwwRestNotes,
     umowchuwowiestNotes,
     umowchuwowiestNotes,
+    // season 2
     umowwwAltNotes,
     umowchuwowiestAltNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
+    // season 3
     umowwwRestNotes,
     rightNumberOfBackboneNotes,
     rightNumberOfBackboneNotes,
+    // season 4
     umowwwRestNotes,
     umowchuwowiestAltNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
 ])
 const umowchuwowiestCrossedWithStepwiseThreeNotes = sequence([
+    // season 1
     umowwwRestNotes,
     umowchuwowiestNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseUmowchuwowiestNotes,
+    // season 2
     umowwwRestNotes,
     umowchuwowiestAltNotes,
     stepwiseForUmowchuwowiestCrossedWithStepwiseUmowchuwowiestNotes,
+    // season 3
     backboneTwentyfourNotes,
     rightNumberOfBackboneNotes,
-    stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
+    stepwiseForUmowchuwowiestCrossedWithStepwiseInbetweenNotes,
+    // season 4
     umowwwAltAndNotNotes,
-    stepwiseForUmowchuwowiestCrossedWithStepwiseStepwiseNotes,
+    stepwiseForUmowchuwowiestCrossedWithStepwiseInbetweenNotes,
     umowchuwowiestNotes,
 ])
 
