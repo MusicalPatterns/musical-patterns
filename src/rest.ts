7472bdb94e8b39241cf8ce3829bcb191b9845745
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-05 14:20:59 -0700

    got head around zdaubyaos

---
+++ src/rest.ts
@@ -0,0 +1,13 @@
+import { PLACEHOLDER_PITCH_TO_AVOID_NULL_POINTER_ISSUES, SEPARATION_FOR_NEIGHBORING_NOTES, SILENT } from './constants'
+import { Note } from './types'
+
+const rest: (_: number) => Note[] =
+    (duration: number): Note[] =>
+        [{
+            duration,
+            gain: SILENT,
+            pitch: PLACEHOLDER_PITCH_TO_AVOID_NULL_POINTER_ISSUES,
+            sustain: duration - SEPARATION_FOR_NEIGHBORING_NOTES,
+        }]
+
+export default rest
