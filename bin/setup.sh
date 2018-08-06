1fc704533fa9fc639c089c8aedf6ec5664680706
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-06 09:07:26 -0700

    better git setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,24 @@
 
 set -e
 
+git config --get user.name > /dev/null 2>&1
+if [[ $? -ne 0 ]] ; then
+    printf "${Yellow}Please set your global git user name: ${NC}"
+    read USER_NAME
+    git config --global user.name $USER_NAME
+fi
+
+git config --get user.email > /dev/null 2>&1
+if [[ $? -ne 0 ]] ; then
+    printf "${Yellow}Please set your global git user email: ${NC}"
+    read USER_EMAIL
+    git config --global user.email $USER_EMAIL
+fi
+
 git submodule update --init --recursive
+git submodule foreach git checkout master
+git submodule foreach git pull -r
+git pull -r
 
 if [[ $OSTYPE == darwin* ]] ; then
     if hash brew 2>/dev/null; then
