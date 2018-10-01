3282cb5014419c8aae5c1599ccf74781c8862fef
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-01 14:10:15 -0700

    xelt

--- src/song.ts
+++ src/song.ts
@@ -1,9 +1,10 @@
 // tslint:disable:no-unused-variable
 import { hafuhafu } from '../songs/hafuhafu/src/songs'
 import { stepwise } from '../songs/stepwise/src/songs'
+import { xeltDleudefAetNaelNaemeugz } from '../songs/xelt-dleudef-aet-naelnaemeugz/src/songs'
 import { zdaubyaos } from '../songs/zdaubyaos/src/songs'
 import { Song } from './types'
 
-const song: Song = hafuhafu
+const song: Song = xeltDleudefAetNaelNaemeugz
 
 export default song
