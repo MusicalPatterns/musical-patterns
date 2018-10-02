7915db64874feab0572eb7b460bb790eb6d9b812
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-02 13:23:51 -0700

    beaten path

--- src/song.ts
+++ src/song.ts
@@ -1,10 +1,11 @@
 // tslint:disable:no-unused-variable
+import { beatenPath } from '../songs/beaten-path/src/songs'
 import { hafuhafu } from '../songs/hafuhafu/src/songs'
 import { stepwise } from '../songs/stepwise/src/songs'
 import { xeltDleudefAetNaelNaemeugz } from '../songs/xelt-dleudef-aet-naelnaemeugz/src/songs'
 import { zdaubyaos } from '../songs/zdaubyaos/src/songs'
 import { Song } from './types'
 
-const song: Song = xeltDleudefAetNaelNaemeugz
+const song: Song = beatenPath
 
 export default song
