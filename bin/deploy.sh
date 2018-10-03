be540823a86eec1be55903a22ce1ec6460d7c546
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-03 15:30:27 -0700

    cf -> gcp

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env sh
 
-cf a > /dev/null
-if [[ $? == 1 ]] ; then
-    if [[ -z "${PWS_ORG+x}" ]] ; then
-        printf "${Cyan}Please set PWS_ORG, PWS_SPACE, PWS_USERNAME, and PWS_PASSWORD environment variables if you would like to automatically push to a Cloud Foundry.\n${NC}"
-        exit 1
-    else
-        cf login -a api.run.pivotal.io -o "$PWS_ORG" -s "$PWS_SPACE" -u "$PWS_USERNAME" -p "$PWS_PASSWORD"
-    fi
-fi
-
 NODE_ENV=production webpack
-cf zero-downtime-push fun-musical-tool -f manifest.yml
+gcloud config configurations activate fun-musical-tool
+gcloud app deploy -q
