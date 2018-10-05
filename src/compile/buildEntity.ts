ed0f81c49e1c4973bd7cac5db539ecfe0385977e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-04 17:31:51 -0700

    more build entity defaults

--- src/compile/buildEntity.ts
+++ src/compile/buildEntity.ts
@@ -1,18 +1,18 @@
 import { Song } from '../songTypes'
-import { Entity, Timbre, Voice, VoiceType } from '../types'
+import { Entity, OscillatorName, Timbre, Voice, VoiceType } from '../types'
 import * as to from '../utilities/to'
 import buildOscillatorVoice from './buildOscillatorVoice'
 import buildSampleVoice from './buildSampleVoice'
-import { EntityConfig } from './types'
+import { EntityConfig, TimeType } from './types'
 
 const buildEntity: (entityConfig: EntityConfig, song: Song) => Entity =
     ({
          nextEnd = to.Time(0),
          nextStart = to.Time(0),
          noteIndex = to.Index(0),
-         notes,
-         timeType,
-         voiceConfig: {voiceType, timbre},
+         notes = [],
+         timeType = TimeType.ATOMIC,
+         voiceConfig: {voiceType, timbre} = {voiceType: VoiceType.OSCILLATOR, timbre: OscillatorName.SQUARE},
          voiceGain = to.Scalar(1),
      }: EntityConfig,
      song: Song): Entity => {
