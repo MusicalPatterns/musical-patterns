c5a51039fd800f7162076688c61b4e743b3e8e80
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-12 10:16:32 -0700

    dont stop song and recompile if nothing changed

--- src/interface/handleConfigSubmit.ts
+++ src/interface/handleConfigSubmit.ts
@@ -1,4 +1,5 @@
 import { Song } from '../songTypes'
+import { deepEqual } from '../utilities/deepEqual'
 import { ActionType } from './actions'
 import { destringifyConfig } from './destringifyConfig'
 import { recompileAndRestart } from './recompileAndRestart'
@@ -9,11 +10,15 @@ import { HandleConfigSubmitParameters } from './types'
 const handleConfigSubmit: (handleConfigSubmitParameters: HandleConfigSubmitParameters) => Promise<void> =
     async (handleConfigSubmitParameters: HandleConfigSubmitParameters): Promise<void> => {
         const { configKey, configValue, dispatch, entities, actualCurrentConfig, song } = handleConfigSubmitParameters
-        stopPreviousSong(entities)
 
         const updatedConfig: InterfaceConfig = { ...actualCurrentConfig, [configKey]: configValue }
+        if (deepEqual(actualCurrentConfig, updatedConfig)) {
+            return
+        }
+
         try {
             const newSong: Song = { ...song, config: destringifyConfig(updatedConfig) }
+            stopPreviousSong(entities)
             dispatch({ type: ActionType.SET_ACTUAL_CURRENT_CONFIG, data: updatedConfig })
             await recompileAndRestart(newSong, dispatch)
         }
