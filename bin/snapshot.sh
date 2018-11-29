cb74c527930030b5334c5a31619fb8bde4edc2da
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-29 09:28:33 -0800

    oops should have been snapshotting from cli

--- bin/snapshot.sh
+++ bin/snapshot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-cp node_modules/@musical-patterns/shared/test/tsconfig.json ./test/
-cp node_modules/@musical-patterns/shared/test/mockDom.ts ./test/
+cp node_modules/@musical-patterns/cli/test/tsconfig.json ./test/
+cp node_modules/@musical-patterns/cli/test/mockDom.ts ./test/
 
 node_modules/.bin/tsc -p test/tsconfig.json
 node_modules/.bin/ts-node -P test/tsconfig.json ./bin/snapshot.js
