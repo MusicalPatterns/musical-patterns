b7328fd374f69d2c1179bfb641c0bd11bca83361
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-09 14:20:33 -0800

    lab can make ship

--- bin/ship.sh
+++ bin/ship.sh
@@ -5,11 +5,6 @@ set -e
 make test
 make lint
 
-commit_recursively() {
-	git add .
-	git commit -m "${MSG}"
-	git submodule foreach commit_recursively
-}
-commit_recursively
+make commit_recursively
 
 make push
