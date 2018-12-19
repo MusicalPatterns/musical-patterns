e464f4b861658dc279bb8ec6c779f8c8fd37d8d2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-18 16:12:57 -0800

    using latest local is more robust

--- bin/update.sh
+++ bin/update.sh
@@ -4,15 +4,20 @@ set -e
 
 update_package() {
 	pushd package
-		make update
+		npm i
+		npm update
 	popd
 }
 export -f update_package
 
-if [[ ${PATTERN} == "ALL" ]] ; then
-	git submodule foreach update_package
+if [[ ${PATTERN} == "" ]] ; then
+	sh node_modules/@musical-patterns/cli/bin/update.sh
 else
-	PATTERN_DIR=src/${PATTERN}
-	cd ${PATTERN_DIR}
-	update_package
+	if [[ ${PATTERN} == "ALL" ]] ; then
+		git submodule foreach update_package
+	else
+		PATTERN_DIR=src/${PATTERN}
+		cd ${PATTERN_DIR}
+		update_package
+	fi
 fi
