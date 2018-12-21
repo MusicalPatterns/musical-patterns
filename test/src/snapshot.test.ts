d5d07334be41b9d42977f99abf1d407695cec16b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 17:31:58 -0800

    standardize make rules for patterns, conforming lint and test

--- test/src/snapshot.test.ts
+++ test/src/snapshot.test.ts
@@ -1,13 +1,15 @@
 import { compilePattern } from '@musical-patterns/compiler'
-import { Pattern, PatternId, patternsFilter } from '@musical-patterns/pattern'
-import { camelCaseToConstantCase } from '@musical-patterns/utilities'
-import { patterns } from '../../src/indexForTest'
+import { Pattern } from '@musical-patterns/pattern'
 
 // tslint:disable-next-line:no-any
 declare const require: any
 
-const snapshotTestPattern: (patternName: string, pattern: Pattern) => Promise<void> =
-    async (patternName: string, pattern: Pattern): Promise<void> => {
+if (!!process.env.PATTERN) {
+    const patternName: string = process.env.PATTERN
+    // tslint:disable-next-line:no-var-requires
+    const pattern: Pattern = require(`../../src/${patternName}`).pattern
+
+    if (pattern) {
         it(`${patternName} stays locked down`, async (done: DoneFn) => {
             expect(JSON.stringify(await compilePattern(pattern), undefined, 2))
             // tslint:disable-next-line:no-require-imports no-unsafe-any
@@ -16,27 +18,7 @@ const snapshotTestPattern: (patternName: string, pattern: Pattern) => Promise<vo
             done()
         })
     }
-
-if (process.env.PATTERN) {
-    const patternName: PatternId = camelCaseToConstantCase(process.env.PATTERN) as PatternId
-    const pattern: Pattern = patterns[ patternName ]
-
-    if (pattern) {
-        snapshotTestPattern(patternName, pattern)
-            .then()
-            .catch()
-    }
     else {
         it('warns you that you tried to focus snapshot test on a pattern that was not found')
     }
-}
-else {
-     describe('snapshot', () => {
-        Object.entries(patternsFilter(patterns))
-            .forEach(([ patternName, pattern ]: [ string, Pattern ]): void => {
-                snapshotTestPattern(patternName, pattern)
-                    .then()
-                    .catch()
-            })
-    })
 }
