f4c8b752732a3ea9c5cd09430bd5315ce7c2bb8d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 16:21:25 -0800

    prefer tsconfig in each pattern over copying and removing it

--- bin/pattern/publish_pattern.sh
+++ bin/pattern/publish_pattern.sh
@@ -5,11 +5,6 @@ publish_pattern() {
 		printf "Please specify a pattern to publish with 'PATTERN=my-pattern'"
 		exit 1
 	fi
-
-	cp ../../bin/pattern/tsconfig.json .
-
 	make publish
-
-	rm tsconfig.json
 }
 export -f publish_pattern
