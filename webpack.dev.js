5f755117f1a22135ec4be9c3303b3deb99547590
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-21 14:01:20 -0800

    for some reason this is now necessary

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ const merge = require('webpack-merge')
 module.exports = merge(common, {
     mode: 'development',
     devServer: {
+        disableHostCheck: true,
         contentBase: path.join(__dirname, './dist'),
     },
     devtool: 'eval-source-map',
