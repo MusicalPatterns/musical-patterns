9fdac8ec26625d1e268aeb9b8d56da28542d06d1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 13:13:37 -0800

    improve update clean

--- bin/update.sh
+++ bin/update.sh
@@ -4,18 +4,13 @@ set -e
 
 . ./bin/pattern/do_for_self_all_or_one.sh
 
-update_with_latest_knowledge() {
-	npm i || return
-	npm update
-}
-export -f update_with_latest_knowledge
-
 update() {
 	if [[ ${PATTERN} == "" ]] ; then
-		update_with_latest_knowledge
+		npm update
 	else
+		make clean_updates DIR=package
 		pushd package
-			update_with_latest_knowledge
+			npm update
 		popd
 	fi
 }
