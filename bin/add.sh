9f2f5d7a2c9e7960326431500d0bac823bccd187
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 10:25:06 -0800

    no longer track the patterns in the lab (except the template pattern)

--- bin/add.sh
+++ bin/add.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 git submodule add --force --name ${PATTERN} git@github.com:MusicalPatterns/pattern-${PATTERN}.git src/${PATTERN}
+git config -f .gitmodules submodule.${PATTERN}.ignore all
 make share_config
