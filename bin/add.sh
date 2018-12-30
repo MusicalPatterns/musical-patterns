dad0479c9f821b4a269568f33dc4ceaea10ca029
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-29 16:53:44 -0800

    maybe stop ignoring modules

--- bin/add.sh
+++ bin/add.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 git submodule add --force --name ${PATTERN} https://github.com/MusicalPatterns/pattern-${PATTERN}.git src/${PATTERN}
-git config -f .gitmodules submodule.${PATTERN}.ignore all
 make share_config
