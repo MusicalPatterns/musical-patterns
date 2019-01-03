e045cfe8889d5f640830ac065d51b4d47937d744
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 09:04:49 -0800

    extract utilities from pattern - now with less tests included in npm package

--- src/selectedPatterns.ts
+++ src/selectedPatterns.ts
@@ -1,7 +1,7 @@
-import { PatternId, Patterns, patternsFilter } from '@musical-patterns/pattern'
+import { filter, PatternId, Patterns } from '@musical-patterns/pattern'
 import { patterns } from './patterns'
 
-let selectedPatterns: Patterns = patternsFilter(patterns)
+let selectedPatterns: Patterns = filter(patterns)
 
 const whitelist: PatternId[] = [
 ]
