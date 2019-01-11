e640b7320ac51f71c0e7a44a3ca04cfa23eafe29
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-10 20:34:07 -0800

    lint issue... ah ha yeah see these tiny few files arent checked

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,7 +1,7 @@
 import { AllPatterns, filter, Patterns } from '@musical-patterns/registry'
 import * as allPatterns from './allPatterns'
 
-let patterns: Patterns = filter(allPatterns as AllPatterns)
+const patterns: Patterns = filter(allPatterns as AllPatterns)
 
 export {
     patterns,
