8d8f3ca493c079835d42dd95a3298f31509059dd
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-19 11:11:08 -0800

    temp shut up ts

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,7 +1,8 @@
 import { filter, Patterns } from '@musical-patterns/registry'
 import * as allPatterns from './allPatterns'
 
-const patterns: Patterns = filter(allPatterns)
+// tslint:disable-next-line:no-any
+const patterns: Patterns = filter(allPatterns as any)
 
 export {
     patterns,
