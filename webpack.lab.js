a927454dfc17bc27fd8f6e4ffc8a88a13549bff2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-26 15:00:18 -0800

    lab manages its own webpack now

---
+++ webpack.lab.js
@@ -0,0 +1,16 @@
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
+module.exports = merge(common, {
+    entry: './src/playroom.ts',
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Musical Patterns',
+            meta: { viewport: 'width=device-width' },
+        }),
+        new FaviconsWebpackPlugin('./node_modules/@musical-patterns/playroom/assets/favicon.png'),
+    ],
+})
