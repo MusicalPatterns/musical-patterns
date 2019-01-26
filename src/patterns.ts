e814e748098e6db749377679dee2cba75a2337d9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 16:17:05 -0800

    i give up on this one, not worth it

--- src/patterns.ts
+++ src/patterns.ts
@@ -1,6 +1,7 @@
 import { filter, Patterns } from '@musical-patterns/pattern'
 import * as allPatterns from './allPatterns'
 
+// @ts-ignore
 const patterns: Patterns = filter(allPatterns)
 
 export {
